4c0883eb5d7172ef9c6134bad9d26a0a8116f53d
Monalisha Mishra <mishramonalisha76@gmail.com>
2022-11-03 17:52:44 +0530

    fix: removed lightweight package

--- packages/restapi/src/lib/chat/helpers/pgp.ts
+++ packages/restapi/src/lib/chat/helpers/pgp.ts
@@ -1,8 +1,7 @@
 import * as openpgp from 'openpgp';
-import { generateKey } from 'openpgp/lightweight';
 
 export const generateKeyPair = async (): Promise<{ privateKeyArmored: string; publicKeyArmored: string }> => {
-  const keys = await generateKey({
+  const keys = await openpgp.generateKey({
     type: 'rsa',
     rsaBits: 2048,
     userIDs: [{ name: '', email: '' }]
