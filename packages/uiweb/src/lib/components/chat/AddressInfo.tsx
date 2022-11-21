0c5ca2157259287f31b310207dc1c9382de5f099
Monalisha Mishra <mishramonalisha76@gmail.com>
2022-11-21 18:03:49 +0530

    refactor: fill added in copy icon

--- packages/uiweb/src/lib/components/chat/AddressInfo.tsx
+++ packages/uiweb/src/lib/components/chat/AddressInfo.tsx
@@ -2,17 +2,17 @@ import React, { useContext, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { ChatPropsContext } from '../../context';
 import * as PushAPI from '@pushprotocol/restapi';
-import {Constants} from '../../config';
+import { Constants } from '../../config';
 import { copyToClipboard, pCAIP10ToWallet } from '../../helpers';
-import {ReactComponent as CopySvg} from "../../icons/chat/copy.svg";
+import { ReactComponent as CopySvg } from '../../icons/chat/copy.svg';
 
 export const AddressInfo: React.FC = () => {
-  const { supportAddress, env,theme } = useContext<any>(ChatPropsContext);
+  const { supportAddress, env, theme } = useContext<any>(ChatPropsContext);
   const [ensName, setEnsName] = useState<string>('');
   const [user, setUser] = useState<any>({});
+  const [isCopied, setIsCopied] = useState<boolean>(false);
   const walletAddress = pCAIP10ToWallet(supportAddress);
-  
- 
+
   useEffect(() => {
     const getUser = async () => {
       const user = await PushAPI.user.get({ account: walletAddress, env });
@@ -24,16 +24,37 @@ export const AddressInfo: React.FC = () => {
   return (
     <Container theme={theme}>
       <Section>
-      <Image src={user?.profilePicture? user?.profilePicture : Constants.DEFAULT_PROFILE_PICTURE } alt="address profile" />
-      <Span theme={theme}>
-        {ensName && `${ensName}`}
-        {!ensName &&
-          `${walletAddress.substring(0, 8)}...${walletAddress.substring(
-            walletAddress.length - 8
-          )}`}
-      </Span>
+        <Image
+          src={
+            user?.profilePicture
+              ? user?.profilePicture
+              : Constants.DEFAULT_PROFILE_PICTURE
+          }
+          alt="address profile"
+        />
+        <Span theme={theme}>
+          {ensName && `${ensName}`}
+          {!ensName &&
+            `${walletAddress.substring(0, 8)}...${walletAddress.substring(
+              walletAddress.length - 8
+            )}`}
+        </Span>
       </Section>
-      <CopySvg onClick={()=>copyToClipboard(walletAddress)} stroke={theme.btnColorSecondary}/>
+      {!isCopied && (
+        <CopySvg
+          onClick={() => {
+            copyToClipboard(walletAddress); setIsCopied(true);
+          }}
+          stroke={theme.btnColorSecondary}
+        />
+      )}
+      {isCopied && (
+        <CopySvg
+          stroke={theme.btnColorSecondary}
+          onMouseLeave={() => setIsCopied(false)}
+          fill={theme.btnColorSecondary}
+        />
+      )}
     </Container>
   );
 };
@@ -41,11 +62,11 @@ export const AddressInfo: React.FC = () => {
 //styles
 const Container = styled.div`
   display: flex;
-  align-items:center;
+  align-items: center;
   justify-content: space-between;
   flex-direction: row;
   box-sizing: border-box;
-  background:${(props) => props.theme.bgColorPrimary || '#fff'};
+  background: ${(props) => props.theme.bgColorPrimary || '#fff'};
   border: ${(props) => props.theme.border};
   padding: 5px 20px 5px 5px;
   margin: 13px 0;
@@ -75,5 +96,5 @@ const Span = styled.span`
   align-items: center;
   line-height: 25px;
   letter-spacing: -0.019em;
-  color:${(props: any): string => props.theme.textColorPrimary || '#000'};
+  color: ${(props: any): string => props.theme.textColorPrimary || '#000'};
 `;
