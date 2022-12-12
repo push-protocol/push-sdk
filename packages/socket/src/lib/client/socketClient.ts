aba9f17c037fe59e44729c9b49c8eaa9e2a71630
strykerin <dacosta.pereirafabio@gmail.com>
2022-12-12 12:18:18 +0530

    feat: add chat wss

--- packages/socket/src/lib/client/socketClient.ts
+++ packages/socket/src/lib/client/socketClient.ts
@@ -6,36 +6,36 @@ import { SocketInputOptions } from '../types';
 export function createSocketConnection({
   user,
   env,
+  socketType = 'notification',
+  apiKey,
   socketOptions
 }: SocketInputOptions
 ) {
-    const {
-      autoConnect = true,
-      reconnectionAttempts = 5,
-    } = socketOptions || {};
-    const epnsWSUrl = API_URLS[env];
-    const transports = ['websocket'];
+  const {
+    autoConnect = true,
+    reconnectionAttempts = 5,
+  } = socketOptions || {};
+  const pushWSUrl = API_URLS[env];
+  const transports = ['websocket'];
 
-    let epnsSocket = null;  
-  
-  
-    try {
-      const userAddressInCAIP = getCAIPAddress(env, user, 'User');
-      // the backend only accepts CAIP
-      const query = { address: userAddressInCAIP };
+  let pushSocket: ReturnType<typeof io> | null = null;
+  try {
+    const userAddressInCAIP = getCAIPAddress(env, user, 'User');
+    let query;
+    if (socketType === 'notification') query = { address: userAddressInCAIP };
+    else query = { mode: 'chat', did: userAddressInCAIP, apiKey }
 
-      epnsSocket = io(epnsWSUrl, {
-        transports,
-        query,
-        autoConnect,
-        reconnectionAttempts,
-      });
-  
-    } catch (e) {
-      console.error('[EPNS-SDK] - Socket connection error: ');
-      console.error(e);
-    } finally {
-      // eslint-disable-next-line no-unsafe-finally
-      return epnsSocket;
-    }
+    pushSocket = io(pushWSUrl, {
+      transports,
+      query,
+      autoConnect,
+      reconnectionAttempts,
+    });
+  } catch (e) {
+    console.error('[EPNS-SDK] - Socket connection error: ');
+    console.error(e);
+  } finally {
+    // eslint-disable-next-line no-unsafe-finally
+    return pushSocket;
+  }
 }
