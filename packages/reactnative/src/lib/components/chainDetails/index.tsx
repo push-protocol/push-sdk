6674036135ca86a3dec7b52acbb9e9e219da6c32
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-01-12 18:39:26 +0530

    feat: added config changes for bsc chain

--- packages/reactnative/src/lib/components/chainDetails/index.tsx
+++ packages/reactnative/src/lib/components/chainDetails/index.tsx
@@ -1,13 +1,14 @@
 import EthereumSvg from "./ethereumSVG";
 import PolygonSvg from "./polygonSVG";
-import GraphSvg from "./thegraphSVG"
+import GraphSvg from "./thegraphSVG";
+import BscSvg from "./bscSVG";
 
 export default {
     ETH_TEST_GOERLI: { label: "ETHEREUM GOERLI", Icon: EthereumSvg },
     ETH_MAINNET: { label: "ETHEREUM MAINNET", Icon: EthereumSvg },
     POLYGON_TEST_MUMBAI: { label: "POLYGON MUMBAI", Icon: PolygonSvg },
     POLYGON_MAINNET: { label: "POLYGON MAINNET", Icon: PolygonSvg },
-    BSC_TESTNET: { label: "BSC TESTNET", Icon: PolygonSvg },
-    BSC_MAINNET: { label: "BSC MAINNET", Icon: PolygonSvg },
+    BSC_TESTNET: { label: "BSC TESTNET", Icon: BscSvg },
+    BSC_MAINNET: { label: "BSC MAINNET", Icon: BscSvg },
     THE_GRAPH: { label: "THE GRAPH", Icon: GraphSvg },
 };
