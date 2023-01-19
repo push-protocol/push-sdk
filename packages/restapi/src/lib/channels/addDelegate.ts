d4334f45428097c92705946df3f30d933f808b2d
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-01-19 14:21:31 +0530

    refactor: add delegate contract call

---
+++ packages/restapi/src/lib/channels/addDelegate.ts
@@ -0,0 +1,34 @@
+import { ethers, Signer } from 'ethers';
+import { abis } from '../config';
+import Constants from '../constants';
+import { getConfig, isValidETHAddress } from '../helpers';
+
+export type AddDelegateOptionsType = {
+  signer: Signer;
+  delegateAddress: string;
+  env?: string;
+};
+
+export const subscribe = async (options: AddDelegateOptionsType) => {
+  const { signer, delegateAddress, env = Constants.ENV.PROD } = options || {};
+  const networkId: number = await signer.getChainId();
+  const { EPNS_COMMUNICATOR_CONTRACT } = getConfig(env, {
+    blockchain: 'eip155',
+    networkId: networkId.toString(),
+  });
+
+  try {
+    if (!isValidETHAddress(delegateAddress)) {
+      throw new Error(`Invalid address!`);
+    }
+    const communicatorSignerInstance = new ethers.Contract(
+      EPNS_COMMUNICATOR_CONTRACT,
+      abis.epnsComm,
+      signer
+    );
+    return communicatorSignerInstance['addDelegate'](delegateAddress);
+  } catch (err) {
+    console.error(`[EPNS-SDK] - Contract ${EPNS_COMMUNICATOR_CONTRACT}: `, err);
+    throw Error(`[EPNS-SDK] - Contract ${EPNS_COMMUNICATOR_CONTRACT}: ${err}`);
+  }
+};
