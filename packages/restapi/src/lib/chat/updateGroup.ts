9f07537d84dc39dc13d756ab4044b44b302cc16a
Shoaib Mohammed <shoaibmohammed92@gmail.com>
2023-02-15 15:05:20 +0530

    fix(component): update group signature generation bug fix

--- packages/restapi/src/lib/chat/updateGroup.ts
+++ packages/restapi/src/lib/chat/updateGroup.ts
@@ -13,7 +13,7 @@ import {
     sign,
     updateGroupRequestValidator
 } from './helpers';
-
+import * as CryptoJS from "crypto-js"
 
 
 /**
@@ -58,14 +58,15 @@ export const updateGroup = async (
         const convertedAdmins = admins.map(walletToPCAIP10);
         const bodyToBeHashed = {
             groupName: groupName,
-            groupImage: groupImage,
             groupDescription: groupDescription,
+            groupImage: groupImage,
             members: convertedMembers,
             admins: convertedAdmins,
             chatId: chatId,
         }
 
-        const signature: string = await sign( {message: JSON.stringify(bodyToBeHashed),  signingKey: connectedUser.privateKey!} );
+        const hash = CryptoJS.SHA256(JSON.stringify(bodyToBeHashed)).toString()
+        const signature: string = await sign( {message: hash,  signingKey: connectedUser.privateKey!} );
         const sigType  = "pgp";
 
         const verificationProof : string = sigType + ":" + signature + ":" + account;
