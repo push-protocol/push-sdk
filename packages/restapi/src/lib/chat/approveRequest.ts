90fb1d5c1a60695d7c559eaa43a6b72b3a5705af
strykerin <dacosta.pereirafabio@gmail.com>
2023-02-22 22:50:28 -0300

    fix: fix merge conflict

--- packages/restapi/src/lib/chat/approveRequest.ts
+++ packages/restapi/src/lib/chat/approveRequest.ts
@@ -5,24 +5,26 @@ import { AccountEnvOptionsType } from '../types';
 import { approveRequestPayload, sign, getConnectedUser, IApproveRequestPayload } from './helpers';
 import * as CryptoJS from "crypto-js"
 
-/**
- *  POST '/v1/chat/request/accept
- */
-
 interface ApproveRequestOptionsType extends AccountEnvOptionsType {
-  senderAddress: string; // chat request sender address
+  /**
+   * Chat request sender address
+   */
+  senderAddress: string;
   pgpPrivateKey?: string,
+  /**
+   * Request state. As of now, only `Approved` is allowed
+  */
   status?: 'Approved';
-  // sigType?: string;
 }
 
+/**
+ * Approve Chat Request
+ */
 export const approve = async (
   options: ApproveRequestOptionsType
 ) => {
   const {
     status = "Approved",
-    // sigType = 'sigType',
-    // privateKey = null,
     account,
     senderAddress,
     env = Constants.ENV.PROD,
@@ -32,34 +34,29 @@ export const approve = async (
   // get user with raw privateKey
   // const createdUser: IConnectedUser = await getConnectedUser(account, privateKey, env);
   // const connectedUser = await createUserIfNecessary({account,env});
-  // TODO: make signature
   const API_BASE_URL = getAPIBaseUrls(env);
   const apiEndpoint = `${API_BASE_URL}/v1/chat/request/accept`;
-
-  const requestUrl = `${apiEndpoint}`;
-
   const body: IApproveRequestPayload = approveRequestPayload(senderAddress, account, status);
 
   const bodyToBeHashed = {
-        fromDID: body.fromDID,
-        toDID: body.toDID,
-        status: body.status
-    }
-    
-    const connectedUser = await getConnectedUser(account, pgpPrivateKey, env);
-    const hash = CryptoJS.SHA256(JSON.stringify(bodyToBeHashed)).toString()
-    const signature: string = await sign({
-        message: hash,
-        signingKey: connectedUser.privateKey!
-    });
-    const sigType = "pgp";
+    fromDID: body.fromDID,
+    toDID: body.toDID,
+    status: body.status
+  }
 
-    const verificationProof: string = sigType + ":" + signature;
-    body.verificationProof = verificationProof;
+  const connectedUser = await getConnectedUser(account, pgpPrivateKey, env);
+  const hash = CryptoJS.SHA256(JSON.stringify(bodyToBeHashed)).toString()
+  const signature: string = await sign({
+    message: hash,
+    signingKey: connectedUser.privateKey!
+  });
+  const sigType = "pgp";
+  const verificationProof: string = sigType + ":" + signature;
+  body.verificationProof = verificationProof;
 
-  return axios.put(requestUrl, body)
+  return axios.put(apiEndpoint, body)
     .catch((err) => {
-      console.error(`[EPNS-SDK] - API ${requestUrl}: `, err);
-      throw Error(`[EPNS-SDK] - API ${requestUrl}: ${err}`);
+      console.error(`[Push SDK] - API ${approve.name}: `, err);
+      throw Error(`[Push SDK] - API ${approve.name}: ${err}`);
     });
 }
