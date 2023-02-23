cfd0a83e34df1d343c622999e00766eb33331d31
Nilesh Gupta <guptanilesh2312@gmail.com>
2023-02-23 17:19:05 +0530

    refactor: added pCAIP to wallet in getWallet method

--- packages/restapi/src/lib/chat/helpers/wallet.ts
+++ packages/restapi/src/lib/chat/helpers/wallet.ts
@@ -1,13 +1,17 @@
-import { walletType } from "../../types";
+import { pCAIP10ToWallet } from "../../helpers";
+import { SignerType, walletType } from "../../types";
 
-export const getWallet = (options: walletType) => {
+export const getWallet = (options: walletType): {
+  account: string | null,
+  signer: SignerType | null
+} => {
   const {
     account,
     signer
   } = options || {};
 
   return {
-    account,
+    account: account ? pCAIP10ToWallet(account) : account,
     signer
   };
 }
