72d8d411155ffce55da14810d7aeb5845035dade
aman035 <guptaaman200115@gmail.com>
2023-03-22 11:48:15 +0530

    feat: mediaURL type message support

--- packages/restapi/src/lib/types/index.ts
+++ packages/restapi/src/lib/types/index.ts
@@ -217,7 +217,7 @@ export interface AccountEnvOptionsType extends EnvOptionsType {
 
 export interface ChatOptionsType extends AccountEnvOptionsType {
   messageContent?: string;
-  messageType?: 'Text' | 'Image' | 'File' | 'GIF';
+  messageType?: 'Text' | 'Image' | 'File' | 'GIF' | 'MediaURL';
   receiverAddress: string;
   pgpPrivateKey?: string;
   connectedUser: IConnectedUser;
@@ -229,7 +229,7 @@ export interface ChatOptionsType extends AccountEnvOptionsType {
 
 export interface ChatSendOptionsType {
   messageContent?: string;
-  messageType?: 'Text' | 'Image' | 'File' | 'GIF';
+  messageType?: 'Text' | 'Image' | 'File' | 'GIF' | 'MediaURL';
   receiverAddress: string;
   pgpPrivateKey?: string;
   /**
