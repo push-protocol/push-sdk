d05c0da42099f3fba28d43e94850c962d5674473
Shoaib Mohammed <shoaibmohammed92@gmail.com>
2023-03-28 19:18:36 +0530

    fix(component): removed chalk log

--- packages/restapi/tests/root.ts
+++ packages/restapi/tests/root.ts
@@ -1,10 +1,8 @@
-import chalk = require('chalk');
-
 export const mochaHooks = {
   // This file is needed to end the test suite.
   afterAll(done: () => void) {
     done();
-    console.log(chalk.bold.green.inverse('     ALL TEST CASES EXECUTED     '));
+    console.log('     ALL TEST CASES EXECUTED     ');
     process.exit(0);
   },
 };
