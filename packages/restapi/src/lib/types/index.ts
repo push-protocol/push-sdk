daefd41e0463d4bfa247c713b0ee2b60eafcf045
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-06-12 19:42:22 +0530

    fix: added types for Env and name in Ifeeds

--- packages/restapi/src/lib/types/index.ts
+++ packages/restapi/src/lib/types/index.ts
@@ -3,6 +3,8 @@ import { IDENTITY_TYPE, NOTIFICATION_TYPE } from '../../lib/payloads/constants';
 import { ENV } from '../constants';
 import { EthEncryptedData } from '@metamask/eth-sig-util';
 
+export type Env = (typeof ENV)[keyof typeof ENV];
+
 // the type for the the response of the input data to be parsed
 export type ApiNotificationType = {
   payload_id: number;
@@ -139,6 +141,7 @@ export interface IFeeds {
   did: string;
   wallets: string;
   profilePicture: string | null;
+  name: string | null;
   publicKey: string | null;
   about: string | null;
   threadhash: string | null;
