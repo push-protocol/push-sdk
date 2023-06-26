9f6720201c173a3732de0559559239cca79d88de
aman035 <guptaaman200115@gmail.com>
2023-06-26 16:50:39 +0530

    fix: added silent notifications

--- packages/restapi/src/lib/types/index.ts
+++ packages/restapi/src/lib/types/index.ts
@@ -7,7 +7,7 @@ import {
 import { ENV } from '../constants';
 import { EthEncryptedData } from '@metamask/eth-sig-util';
 
-export type Env = (typeof ENV)[keyof typeof ENV];
+export type Env = typeof ENV[keyof typeof ENV];
 
 // the type for the the response of the input data to be parsed
 export type ApiNotificationType = {
@@ -91,7 +91,9 @@ export interface ISendNotificationInputOptions {
     body: string;
     cta: string;
     img: string;
-    metadata?: any;
+    hidden?: boolean;
+    etime?: number;
+    silent?: boolean;
     additionalMeta?:
       | {
           /**
@@ -103,10 +105,23 @@ export interface ISendNotificationInputOptions {
           domain?: string;
         }
       | string;
+    /**
+     * @deprecated
+     * use additionalMeta instead
+     */
+    metadata?: any;
   };
   recipients?: string | string[]; // CAIP or plain ETH
   channel: string; // CAIP or plain ETH
+  /**
+   * @deprecated
+   * use payload.etime instead
+   */
   expiry?: number;
+  /**
+   * @deprecated
+   * use payload.hidden instead
+   */
   hidden?: boolean;
   graph?: {
     id: string;
