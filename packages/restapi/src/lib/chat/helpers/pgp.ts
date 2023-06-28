802489ed91df3cd1206c7690e6f208d20ff1934c
kalashshah <202051096@iiitvadodara.ac.in>
2023-06-28 15:31:01 +0530

    refactor: pgp and crypto helper functions

--- packages/restapi/src/lib/chat/helpers/pgp.ts
+++ packages/restapi/src/lib/chat/helpers/pgp.ts
@@ -1,7 +1,9 @@
 import * as openpgp from 'openpgp';
 
 interface IPGPHelper{
-  generateKeyPair(): Promise<{ privateKeyArmored: string; publicKeyArmored: string }>
+  generateKeyPair(): Promise<{ privateKeyArmored: string; publicKeyArmored: string }>;
+  sign ({ message, signingKey }: { message: string; signingKey: string }): Promise<string>;
+  pgpEncrypt ({ plainText, keys }: { plainText: string; keys: Array<string> }): Promise<string>;
 }
 
 const PGPHelper:IPGPHelper = {
@@ -15,7 +17,27 @@ const PGPHelper:IPGPHelper = {
       privateKeyArmored: keys.privateKey,
       publicKeyArmored: keys.publicKey
     }
-  }
+  },
+
+  async sign ({ message, signingKey }): Promise<string> {
+    const messageObject: openpgp.Message<string> = await openpgp.createMessage({ text: message })
+    const privateKey: openpgp.PrivateKey = await openpgp.readPrivateKey({ armoredKey: signingKey })
+    return <string>await openpgp.sign({ message: messageObject, signingKeys: privateKey, detached: true })
+  },
+
+  async pgpEncrypt ({ plainText, keys }): Promise<string> {
+    const pgpKeys: openpgp.Key[] = [];
+  
+    for(let i = 0; i < keys.length; i++) {
+      pgpKeys.push(await openpgp.readKey({ armoredKey: keys[i] }));
+    }
+    const message: openpgp.Message<string> = await openpgp.createMessage({ text: plainText });
+    const encrypted: string = <string>await openpgp.encrypt({
+      message: message,
+      encryptionKeys: pgpKeys,
+    });
+    return encrypted;
+  },
 }
 
 export {IPGPHelper, PGPHelper}
