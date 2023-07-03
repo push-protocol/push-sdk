6ce1a73381f5ccb14a12d576c7a47108b9d3a02f
Arnab Chatterjee <60937304+arn4b@users.noreply.github.com>
2023-07-03 16:26:36 +0530

    refactor: spaces UI Components refactor (#478)
    > replaced hardcoded styles with context theme >added smoother animations on collapsoble components
    >added profile cards to invite modal

--- packages/uiweb/src/lib/components/space/SpaceWidget/WidgetContent.tsx
+++ packages/uiweb/src/lib/components/space/SpaceWidget/WidgetContent.tsx
@@ -9,6 +9,7 @@ const SCHEDULED_WIDGET_CONTENT_FIXED_HEIGHT = '350px';
 
 interface WidgetContentProps {
   shareUrl?: string;
+  isMinimized: boolean;
 
   // temp props only for testing demo purpose for now
   isHost?: boolean;
@@ -17,9 +18,12 @@ interface WidgetContentProps {
   isTimeToStartSpace? :boolean;
   isMember?: boolean;
 }
-export const WidgetContent: React.FC<WidgetContentProps> = ({ shareUrl, isHost, isLive, isJoined, isTimeToStartSpace, isMember }: WidgetContentProps) => {
+export const WidgetContent: React.FC<WidgetContentProps> = ({ shareUrl, isHost, isLive, isJoined, isTimeToStartSpace, isMember, isMinimized }: WidgetContentProps) => {
   return (
-    <Container height={isLive ? LIVE_WIDGET_CONTENT_FIXED_HEIGHT : SCHEDULED_WIDGET_CONTENT_FIXED_HEIGHT}>
+    <Container
+      isMinimized={isMinimized}
+      height={isLive ? LIVE_WIDGET_CONTENT_FIXED_HEIGHT : SCHEDULED_WIDGET_CONTENT_FIXED_HEIGHT}
+    >
       {isLive
         ?
           <LiveWidgetContent 
@@ -39,11 +43,15 @@ export const WidgetContent: React.FC<WidgetContentProps> = ({ shareUrl, isHost, 
 };
 
 //styles
-const Container = styled.div<{height: string}>`
+const Container = styled.div<{height: string, isMinimized: boolean}>`
   display: flex;
   flex-direction: column;
   border-bottom: ${(props) => props.theme.border};
-  height: ${(props) => props.height};
+
+  height: ${(props) => (props.isMinimized ? '0' : props.height)};
+  transition: height 200ms ease-out;
+  overflow: hidden;
+  
   align-items: center;
   justify-content: space-between;
 `;
--- packages/uiweb/src/lib/components/space/reusables/Modal.tsx
+++ packages/uiweb/src/lib/components/space/reusables/Modal.tsx
@@ -3,43 +3,51 @@
  * generic modal component for spaces UI
  * does not handle any business logic, acts only as a container
  */
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 import styled from 'styled-components'
+
+import { ThemeContext } from '../theme/ThemeProvider';
 
 import { useClickAway } from '../../../hooks';
 
 interface IModalProps {
-  width?: string;
-  clickawayClose?: () => void;
-  children: any;
+    width?: string;
+    clickawayClose?: () => void;
+    children: any;
 }
 
 const ClickawayCloseModal = ({ children, clickawayClose, width }: IModalProps) => {
-  const modalRef = useRef(null);
+    const modalRef = useRef(null);
 
-  useClickAway(modalRef, () => {
-    if (clickawayClose) {
-      clickawayClose();
-    }
-  });
+    useClickAway(modalRef, () => {
+        if (clickawayClose) {
+        clickawayClose();
+        }
+    });
 
-  return (
-    <ModalParent ref={modalRef} width={width}>
-      {children}
-    </ModalParent>
-  );
+    return (
+        <ModalParent ref={modalRef} width={width}>
+        {children}
+        </ModalParent>
+    );
 };
 
 export const Modal = ({ clickawayClose, children, width }: IModalProps) => {
-  return (
-    <ModalOverlay>
-      {clickawayClose ? (
-        <ClickawayCloseModal clickawayClose={clickawayClose} width={width}>{children}</ClickawayCloseModal>
-      ) : (
-        <ModalParent width={width}>{children}</ModalParent>
-      )}
-    </ModalOverlay>
-  );
+    const theme = useContext(ThemeContext)
+    return (
+        <ModalOverlay>
+        {clickawayClose ? (
+            <ClickawayCloseModal clickawayClose={clickawayClose} width={width}>{children}</ClickawayCloseModal>
+        ) : (
+            <ModalParent
+                width={width}
+                theme={theme}
+            >
+                { children }
+            </ModalParent>
+        )}
+        </ModalOverlay>
+    );
 };
 
 /* styling */
@@ -50,7 +58,7 @@ const ModalOverlay = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.4); /* Black with 50% opacity */
+    background-color: rgba(0, 0, 0, 0.4); /* Black with 40% opacity */
     display: flex;
     justify-content: center;
     align-items: center;
@@ -67,7 +75,7 @@ const ModalParent = styled.div<IModalProps>`
     align-items: center;
     padding: 24px 20px;
 
-    background: #FFFFFF;
+    background: ${(props => props.theme.bgColorPrimary)};
     border-radius: 12px;
 
     width: ${(props => props.width ? props.width : 'auto')};
