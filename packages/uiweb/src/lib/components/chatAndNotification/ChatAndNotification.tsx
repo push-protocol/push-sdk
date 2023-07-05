03ea5abcbb081063f224579394f3e52c8f207fa4
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-07-05 12:44:52 +0530

    fix: fixed a minor padding

--- packages/uiweb/src/lib/components/chatAndNotification/ChatAndNotification.tsx
+++ packages/uiweb/src/lib/components/chatAndNotification/ChatAndNotification.tsx
@@ -195,7 +195,7 @@ export const ChatAndNotification = () => {
       maxHeight="600px"
       position="fixed"
       background="#fff"
-      padding='0 20px 0 22px'
+      padding='0 20px 0 21px'
       right="12px"
       bottom="18px"
       overflow="hidden"
