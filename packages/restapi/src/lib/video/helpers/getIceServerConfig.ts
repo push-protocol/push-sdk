bf9db46a56cdee7539776e65e8c89a377a96f263
Madhur Gupta <madhur@push.org>
2023-07-07 18:27:28 +0530

    feat(turn-server): add decryption in getIceServerCconfig

--- packages/restapi/src/lib/video/helpers/getIceServerConfig.ts
+++ packages/restapi/src/lib/video/helpers/getIceServerConfig.ts
@@ -2,9 +2,18 @@ import axios from 'axios';
 import { getAPIBaseUrls } from '../../helpers';
 import Constants from '../../constants';
 
+const ENCRYPTION_KEY = 'turnserveradmin';
+
 export const getIceServerConfig = async (env = Constants.ENV.PROD) => {
   const API_BASE_URL = getAPIBaseUrls(env);
   const apiEndpoint = `${API_BASE_URL}/v1/turnserver/iceconfig`;
-  const { data } = await axios.get(apiEndpoint);
-  return data;
+  const { data: encryptedData } = await axios.get(apiEndpoint);
+  const decryptedData = CryptoJS.AES.decrypt(
+    encryptedData,
+    ENCRYPTION_KEY
+  ).toString(CryptoJS.enc.Utf8);
+
+  console.log('encryptedData', encryptedData, 'decryptedData', decryptedData);
+
+  return decryptedData;
 };
