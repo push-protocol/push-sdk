d8c65da472c3c00930a1975c08c31857ce38e509
Arnab Chatterjee <60937304+arn4b@users.noreply.github.com>
2023-07-08 18:56:05 +0530

    feat: add invite functionality to create space (#506)
    * feat: added invite functionality
    
    * fix: add logic for time selector
    
    * fix: css fixes

--- packages/uiweb/src/lib/components/space/SpaceCreationWidget/SCWInviteModal/SCWInviteModal.tsx
+++ packages/uiweb/src/lib/components/space/SpaceCreationWidget/SCWInviteModal/SCWInviteModal.tsx
@@ -1,5 +1,6 @@
-import React, { useState, MouseEventHandler } from 'react'
+import React, { useState, MouseEventHandler, useContext } from 'react'
 import styled from 'styled-components'
+import * as PushAPI from '@pushprotocol/restapi';
 
 import CircularProgressSpinner from '../../../loader/loader';
 
@@ -8,43 +9,92 @@ import { Modal } from '../../reusables/Modal';
 import { Button } from '../../reusables/Button';
 import { SearchInput } from '../../reusables/SearchInput';
 import { ProfileContainer } from '../../reusables/ProfileContainer';
+import { ThemeContext } from '../../theme/ThemeProvider';
+import { SpaceDataContext } from 'packages/uiweb/src/lib/context';
+import { Spinner } from '../../reusables/Spinner';
 
 export interface ISCWIModalProps { // Space Creation Widget Create Modal Interface
     closeInviteModal?: MouseEventHandler;
     makeScheduleVisible?: MouseEventHandler;
     createSpace?: MouseEventHandler;
     isLoading?: boolean;
+    tempMembers?: any;
+    setTempMembers?: any;
+    invitedMembersList?: any;
+    setInvitedMembersList?: any;
+    onClose: () => void;
+}
+
+interface User {
+    handle: string;
+    name: string;
 }
 
 export const SCWInviteModal: React.FC<ISCWIModalProps> = (props) => {
-    const { closeInviteModal, makeScheduleVisible, createSpace, isLoading } = props;
+    const {
+        closeInviteModal, makeScheduleVisible, createSpace, isLoading,
+        tempMembers,
+        setTempMembers,
+        invitedMembersList,
+        setInvitedMembersList,
+        onClose
+    } = props;
+
+    const { env } = useContext(SpaceDataContext)
+
+    const theme = useContext(ThemeContext);
 
     const [invitedMember, setInvitedMember] = useState('')
+    const [loadingAccount, setLoadingAccount] = useState(false)
 
-    const searchMember = (event: any) => {
+    const [searchedUser, setSearchedUser]= useState<any>({});
+
+    const searchMember = async (event: any) => {
         setInvitedMember(event.target.value)
+
+        try {
+            setLoadingAccount(true);
+            const response = await PushAPI.user.get({
+                account: event.target.value,
+                env,
+            });
+            
+            setSearchedUser(response);
+            console.log(response);
+        } catch (e:any) {
+            console.error(e.message);
+        } finally {
+            setLoadingAccount(false);
+        }
     }
 
     const clearInput = () => {
         setInvitedMember('');
     }
 
-    const tempImageUrl = "https://imgv3.fotor.com/images/blog-richtext-image/10-profile-picture-ideas-to-make-you-stand-out.jpg";
+    const handleInviteUser = (index: any) => {
+        const user = tempMembers[index];
 
-    const TEMP_MEMBERS = [
-        {
-            handle: 's4m4',
-            name: 'Samarendra'
-        },
-        {
-            handle: 'aamsa',
-            name: 'Aam Saltman'
-        },
-    ]
+        const updatedTempArray = [...tempMembers];
+        updatedTempArray.splice(index, 1);
+        setTempMembers(updatedTempArray);
+
+        setInvitedMembersList([...invitedMembersList, user]);
+    };
+
+    const handleDeleteInvitedUser = (index: number) => {
+        const updatedArray = [...invitedMembersList];
+        updatedArray.splice(index, 1);
+        setInvitedMembersList(updatedArray);
+    };
+
+    const tempImageUrl = "https://imgv3.fotor.com/images/blog-richtext-image/10-profile-picture-ideas-to-make-you-stand-out.jpg";
 
     return (
         <div>
-            <Modal>
+            <Modal
+                clickawayClose={onClose}
+            >
                 <ModalHeader
                     heading='Invite members'
                     backCallback={makeScheduleVisible}
@@ -60,33 +110,57 @@ export const SCWInviteModal: React.FC<ISCWIModalProps> = (props) => {
 
                 <MembersList>
                     {
-                        TEMP_MEMBERS.map((item) => {
+                        loadingAccount ?? <CircularProgressSpinner />
+                    }
+                    {
+                        Object.keys(searchedUser).length === 0 ?
+                        null
+                        : <ProfileContainer
+                            imageHeight='48px'
+                            handle={searchedUser.wallets.substring(7)}
+                            // handle='test'
+                            name='Test'
+                            imageUrl={searchedUser.profile.picture}
+                            contBtn='Add +'
+                            // btnCallback={() => handleInviteUser()}
+                            border
+                        />
+                    }
+                    {
+                        tempMembers.map((item: any, index: any) => {
                             return <ProfileContainer
                                         imageHeight='48px'
                                         handle={item.handle}
                                         name={item.name}
                                         imageUrl={tempImageUrl}
-                                        tag='Add +'
+                                        contBtn='Add +'
+                                        btnCallback={() => handleInviteUser(index)}
                                         border
                                     />  
                         })
                     }
                 </MembersList>
 
-                <InvitedList>
-                    {
-                        TEMP_MEMBERS.map((item) => {
-                            return <ProfileContainer
-                                        imageHeight='48px'
-                                        handle={item.handle}
-                                        name={item.name}
-                                        imageUrl={tempImageUrl}
-                                        tag='Add +'
-                                        border
-                                    />  
-                        })
-                    }
-                </InvitedList>
+                {
+                    invitedMembersList.length ?
+                    <InvitedList>
+                        <Heading>Invited Members <PendingCount theme={theme}>{invitedMembersList.length}</PendingCount></Heading>
+                        {
+                            invitedMembersList.map((item: any, index: number) => {
+                                return <ProfileContainer
+                                            imageHeight='48px'
+                                            handle={item.handle}
+                                            name={item.name}
+                                            imageUrl={tempImageUrl}
+                                            contBtn='x'
+                                            btnCallback={() => handleDeleteInvitedUser(index)}
+                                            border
+                                        />  
+                            })
+                        }
+                    </InvitedList>
+                    : null
+                }
 
                 <Button
                     onClick={createSpace}
@@ -94,7 +168,7 @@ export const SCWInviteModal: React.FC<ISCWIModalProps> = (props) => {
                 >
                     {
                         isLoading ?
-                        <CircularProgressSpinner />
+                        <Spinner />
                         : 'Create Space'
                     }
                 </Button>
@@ -118,4 +192,18 @@ const InvitedList = styled.div`
     gap: 8px;
 
     margin-top: 28px;
+`;
+
+const Heading = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const PendingCount = styled.div`
+    background: ${(props => props.theme.btnColorPrimary)};
+    border-radius: 8px;
+    padding: 4px 10px;
+    margin-left: 6px;
+    font-size: 13px;
+    color: ${(props => props.theme.titleTextColor)};
 `;
