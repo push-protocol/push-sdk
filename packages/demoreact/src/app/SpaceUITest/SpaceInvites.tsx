b222a6a3123dc9530a2b41cc5efb6e848b5fbc50
aman035 <guptaaman200115@gmail.com>
2023-07-21 12:43:35 +0530

    docs: moved demoreact

--- packages/demoreact/src/app/SpaceUITest/SpaceInvites.tsx
+++
@@ -1,9 +0,0 @@
-import { useSpaceComponents } from "./useSpaceComponents"
-
-export const SpaceInvitesComponent = () => {
-    const { SpaceInvitesComponent } = useSpaceComponents();
-
-    return (
-      <SpaceInvitesComponent />
-    )
-}
