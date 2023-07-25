fa88076d2dbfe33d80f7b5bbaf8491baf774ab5e
Arnab Chatterjee <arnabc1701@gmail.com>
2023-07-25 10:30:39 +0530

    feat: add invite to listener

--- packages/uiweb/src/lib/components/space/SpaceWidget/LiveWidgetContent.tsx
+++ packages/uiweb/src/lib/components/space/SpaceWidget/LiveWidgetContent.tsx
@@ -40,8 +40,6 @@ export const LiveWidgetContent: React.FC<LiveWidgetContentProps> = ({
 
   const [isRequestedForMic, setIsRequestedForMic] = useState(false);
 
-  const dropdownRef = useRef<HTMLDivElement>(null);
-
   const theme = useContext(ThemeContext);
 
   const {
