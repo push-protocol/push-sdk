5a3c3c9cc00e6b0e2d4aed110263d5163ddd2d4e
Satyam <100528412+KlausMikhaelson@users.noreply.github.com>
2023-08-02 17:44:26 +0530

    Chat dataprovider (#596)
    * feat: data provider for chat component
    
    * fix: replaced react.usestate to usestate
    
    * fix: added props as the initial state and changed state name
    
    * fix: reverted chat context changes and renamed values

---
+++ packages/uiweb/src/lib/dataProviders/ChatDataProvider.tsx
@@ -0,0 +1,33 @@
+import { useState, ReactNode } from "react";
+import { ENV } from "../config";
+import { ChatDataContext, IChatDataContextValues } from "../context/chatContext";
+
+export interface IChatUIProviderProps {
+    children: ReactNode;
+    // theme: IChatTheme
+    account: string;
+    pgpPrivateKey: string;
+    env: ENV;
+}
+
+export const ChatUIProvider = ({ children, account, pgpPrivateKey, env }: IChatUIProviderProps) => {
+    // will replace the default values with the ChatUI
+    const [accountVal, setAccountVal] = useState<string>(account);
+    const [pgpPrivateKeyVal, setPgpPrivateKeyVal] = useState<string>(pgpPrivateKey);
+    const [envVal, setEnvVal] = useState<ENV>(env);
+
+    const value: IChatDataContextValues = {
+        account: accountVal,
+        setAccount: setAccountVal,
+        pgpPrivateKey: pgpPrivateKeyVal,
+        setPgpPrivateKey: setPgpPrivateKeyVal,
+        env: envVal,
+        setEnv: setEnvVal,
+    };
+
+    return (
+        <ChatDataContext.Provider value={value}>
+            {children}
+        </ChatDataContext.Provider>
+    )
+}
