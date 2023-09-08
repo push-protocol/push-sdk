68ab9cdfcc4e8cb685b971eec1523f748fadbdf6
Arnab Chatterjee <arnabc1701@gmail.com>
2023-09-08 11:42:45 +0530

    feat: add peerConnections property (fr)

--- packages/restapi/src/lib/spaceV2/SpaceV2.ts
+++ packages/restapi/src/lib/spaceV2/SpaceV2.ts
@@ -10,7 +10,7 @@ export class SpaceV2 {
     protected env: ENV;
     protected callType: VIDEO_CALL_TYPE;
 
-    // protected data: VideoCallData;
+    private peerConnections: Map<string, RTCPeerConnection> = new Map();
 
     constructor({
         signer,
@@ -30,6 +30,34 @@ export class SpaceV2 {
         this.pgpPrivateKey = pgpPrivateKey;
         this.env = env;
         this.callType = callType;
+    }
+
+    // Add a connected peer to the space
+    addPeer(peerId: string, peerConnection: RTCPeerConnection) {
+        if (!this.peerConnections.has(peerId)) {
+            this.peerConnections.set(peerId, peerConnection);
+        } else {
+            console.error(`Peer with ID ${peerId} already exists.`);
+        }
+    }
+
+    // Remove a connected peer from the space
+    removePeer(peerId: string) {
+        if (this.peerConnections.has(peerId)) {
+            this.peerConnections.delete(peerId);
+        } else {
+            console.error(`Peer with ID ${peerId} does not exist.`);
+        }
+    }
+
+    // Get a connected peer's peer connection by their ID
+    getPeerConnection(peerId: string): RTCPeerConnection | undefined {
+        return this.peerConnections.get(peerId);
+    }
+
+    // Get the list of all connected peer IDs
+    getConnectedPeerIds(): string[] {
+        return Array.from(this.peerConnections.keys());
     }
 
     async connect(options: any) {
