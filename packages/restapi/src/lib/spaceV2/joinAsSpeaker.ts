3a03c892378bfb76b02f88701f26ca190383780d
Arnab Chatterjee <arnabc1701@gmail.com>
2023-09-11 17:28:20 +0530

    fix: rm updation

--- packages/restapi/src/lib/spaceV2/joinAsSpeaker.ts
+++ packages/restapi/src/lib/spaceV2/joinAsSpeaker.ts
@@ -3,7 +3,6 @@
  * defines a function that allows a user to join a space as a speaker in the SpaceV2 implmentation
  */
 
-import { get } from './get';
 import { SpaceV2 } from './SpaceV2';
 import sendMetaMessage from './helpers/sendMetaMessage'
 
@@ -22,17 +21,6 @@ export async function joinAsSpeaker(this: SpaceV2) {
         });
 
         /* rest logic to be implemented here after host has called the approve/reject call */
-
-        const updatedSpace = await get({
-            spaceId: this.data.spaceInfo.spaceId,
-            env: this.env,
-        });
-
-        // update space specific data
-        this.setSpaceV2Data(() => ({
-            ...this.data,
-            spaceInfo: updatedSpace
-        }));
     } catch (err) {
         console.error(`[Push SDK] - API  - Error - API ${joinAsSpeaker.name} -:  `, err);
         throw Error(`[Push SDK] - API  - Error - API ${joinAsSpeaker.name} -: ${err}`);
