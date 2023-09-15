61c6bfe53cce0983262c880ff0a34e939c42ef25
Arnab Chatterjee <arnabc1701@gmail.com>
2023-09-15 15:39:23 +0530

    refactor: changed incoming to pending array

--- packages/restapi/src/lib/spaceV2/connect.ts
+++ packages/restapi/src/lib/spaceV2/connect.ts
@@ -25,29 +25,29 @@ export async function connect(
 
     try {
         const peerConnection = this.getPeerConnection(
-            peerAddress ? peerAddress : this.data.incomingPeerStreams[0].address
+            peerAddress ? peerAddress : this.data.pendingPeerStreams[0].address
         ) as any;
 
         peerConnection?.on('error', (err: any) => {
             console.log('error in connect', err);
 
             const incomingIndex = peerAddress
-                ? getIncomingIndexFromAddress(this.data.incomingPeerStreams, peerAddress)
+                ? getIncomingIndexFromAddress(this.data.pendingPeerStreams, peerAddress)
                 : 0;
 
-            if (this.data.incomingPeerStreams[incomingIndex].retryCount >= 5) {
+            if (this.data.pendingPeerStreams[incomingIndex].retryCount >= 5) {
                 console.log('Max retries exceeded, please try again.');
                 this.disconnect({
                     peerAddress: peerAddress
                         ? peerAddress
-                        : this.data.incomingPeerStreams[0].address,
+                        : this.data.pendingPeerStreams[0].address,
                 });
             }
 
             // retrying in case of connection error
             this.request({
                 senderAddress: this.data.local.address,
-                recipientAddress: this.data.incomingPeerStreams[incomingIndex].address,
+                recipientAddress: this.data.pendingPeerStreams[incomingIndex].address,
                 chatId: this.data.spaceInfo.spaceId,
                 retry: true,
             });
@@ -59,9 +59,9 @@ export async function connect(
         this.setSpaceV2Data((oldSpaceData) => {
             return produce(oldSpaceData, (draft) => {
                 const incomingIndex = peerAddress
-                    ? getIncomingIndexFromAddress(oldSpaceData.incomingPeerStreams, peerAddress)
+                    ? getIncomingIndexFromAddress(oldSpaceData.pendingPeerStreams, peerAddress)
                     : 0;
-                draft.incomingPeerStreams[incomingIndex].status = VideoCallStatus.CONNECTED;
+                draft.pendingPeerStreams[incomingIndex].status = VideoCallStatus.CONNECTED;
             });
         });
     } catch (err) {
