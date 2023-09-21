bc95b97e217a02dc1e4dada0e2ed29577af636ce
akp111 <pradhanashish95@gmail.com>
2023-09-21 19:34:25 +0530

    fix: removed comment

--- packages/restapi/tests/lib/pushapi/notification/channel.test.ts
+++ packages/restapi/tests/lib/pushapi/notification/channel.test.ts
@@ -245,7 +245,8 @@ describe('PushAPI.channel functionality', () => {
         url: 'https://google.com',
         icon: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAz0lEQVR4AcXBsU0EQQyG0e+saWJ7oACiKYDMEZVs6GgSpC2BIhzRwAS0sgk9HKn3gpFOAv3v3V4/3+4U4Z1q5KTy42Ql940qvFONnFSGmCFmiN2+fj7uCBlihpgh1ngwcvKfwjuVIWaIGWKNB+GdauSk8uNkJfeNKryzYogZYoZY40m5b/wlQ8wQM8TayMlKeKcaOVkJ71QjJyuGmCFmiDUe+HFy4VyEd57hx0mV+0ZliBlihlgL71w4FyMnVXhnZeSkiu93qheuDDFDzBD7BcCyMAOfy204AAAAAElFTkSuQmCC',
       });
-      console.log(res)
+    //   console.log(res)
+    expect(res).not.null
     }, 10000000000);
   });
 });
