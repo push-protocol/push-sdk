b03178901e64561dd21d075324e12189b41334f9
Arnab Chatterjee <60937304+arn4b@users.noreply.github.com>
2023-10-05 16:55:02 +0530

    Arnab/livekit fixes (#754)
    * refactor: added code for joining livekit room for listeners (#731)
    
    * feat: drop in livekit inplace of livepeer
    
    * feat: added microphone
    
    * feat: added mic
    
    * feat: added access control
    
    * feat: fixes for livekit API call
    
    ---------
    
    Co-authored-by: Nilesh Gupta <guptanilesh2312@gmail.com>

--- packages/uiweb/src/lib/components/space/SpaceWidget/LiveWidgetContent.tsx
+++ packages/uiweb/src/lib/components/space/SpaceWidget/LiveWidgetContent.tsx
@@ -63,6 +63,7 @@ export const LiveWidgetContent: React.FC<LiveWidgetContentProps> = ({
     initSpaceObject,
     raisedHandInfo,
   } = useSpaceData();
+  console.log("🚀 ~ file: LiveWidgetContent.tsx:66 ~ raisedHandInfo:", raisedHandInfo)
 
   const isMicOn = spaceObjectData?.connectionData?.local?.audio;
 
@@ -109,7 +110,7 @@ export const LiveWidgetContent: React.FC<LiveWidgetContentProps> = ({
   useEffect(() => {
     // if (!spaceObjectData?.connectionData?.local?.stream || promotedListener.length === 0)
     //   return;
-    if (promotedListener.length === 0)
+    if (promotedListener.length === 0 || Object.keys(raisedHandInfo).length === 0)
       return;
 
     const options = {
@@ -129,6 +130,7 @@ export const LiveWidgetContent: React.FC<LiveWidgetContentProps> = ({
 
   const handleAcceptPromotion = async (requesterAddress: any) => {
     // await spacesObjectRef?.current?.createAudioStream?.();
+    await performAction({ roomId: spaceData?.spaceId, userId: requesterAddress, canPublish: true });
     setPromotedListener(requesterAddress);
     await performAction({ roomId: spaceData?.spaceId, userId: requesterAddress, canPublish: true });
   };
