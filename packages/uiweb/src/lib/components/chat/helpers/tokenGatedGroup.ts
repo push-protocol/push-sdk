a1c04e1e061bb0e8732cc2cba5ec79c20303ad85
Monalisha Mishra <mishramonalisha76@gmail.com>
2023-10-10 13:57:36 +0530

    fix: added validation for guild

--- packages/uiweb/src/lib/components/chat/helpers/tokenGatedGroup.ts
+++ packages/uiweb/src/lib/components/chat/helpers/tokenGatedGroup.ts
@@ -1,4 +1,5 @@
-import { CriteriaStateType, Data } from '../types';
+import axios from 'axios';
+import { CriteriaStateType, Data, GuildData, Rule } from '../types';
 
 const handleDefineCondition = (
   entryCriteria: CriteriaStateType,
@@ -17,7 +18,50 @@ const handleDefineCondition = (
   }
 };
 
+const validateGUILDData = async (condition: Rule): Promise<string[]> => {
+  const { data } = condition;
+  const errors: string[] = [];
 
+  // Check for guild ID presence
+  if (!(data as GuildData).id) {
+    errors.push('Guild ID is missing');
+  } else {
+    try {
+      const response = await axios.get(
+        `https://api.guild.xyz/v1/guild/${(data as GuildData).id}`
+      );
 
+      if (response.status !== 200) {
+        errors.push('Invalid Guild ID');
+      } else {
+        // Validate the role values
+        if ((data as GuildData).role === '*') {
+          if (data.comparison !== 'all' && data.comparison !== 'any') {
+            errors.push('Invalid comparison value');
+          }
+        } else if ((data as GuildData).role) {
+          const roleExists = response.data.roles.some(
+            (role: { id: number }) =>
+              role.id.toString() === (data as GuildData).role
+          );
+          if (!roleExists) {
+            errors.push('Invalid Guild Role ID');
+          }
 
-export {handleDefineCondition}
+          // For specific role, comparison can be null or empty
+          if (data.comparison) {
+            errors.push('Comparison should be empty for specific role');
+          }
+        } else {
+          errors.push('Invalid role value');
+        }
+      }
+    } catch (error) {
+      errors.push('Error validating Guild ID');
+    }
+  }
+
+  return errors;
+};
+
+export { handleDefineCondition, validateGUILDData };
