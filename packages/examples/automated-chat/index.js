0d07acc533f9e872ce2eb8dcc495757502be01a7
harshrajat <harsh@epns.io>
2023-11-13 15:24:03 +0400

    chore(/packages/examples/automated-chat): tweaked automared chat

--- packages/examples/automated-chat/index.js
+++ packages/examples/automated-chat/index.js
@@ -1,37 +1,40 @@
-import { PushAPI } from '@pushprotocol/restapi';
-import { createSocketConnection, EVENTS } from '@pushprotocol/socket';
+import { CONSTANTS, PushAPI } from '@pushprotocol/restapi';
+import { EVENTS, createSocketConnection } from '@pushprotocol/socket';
 import { ethers } from 'ethers';
 
 // Creating a random signer from a wallet, ideally this is the wallet you will connect
 const signer = ethers.Wallet.createRandom();
 
 // Initialize wallet user, pass 'prod' instead of 'staging' for mainnet apps
-const userAlice = await PushAPI.initialize(signer, { env: 'prod' });
+const userAlice = await PushAPI.initialize(signer, { env: CONSTANTS.ENV.PROD });
 
 // This will be the wallet address of the recipient
 const pushAIWalletAddress = '0x99A08ac6254dcf7ccc37CeC662aeba8eFA666666';
 
-// Create Socket to Listen to incoming messages
-const pushSDKSocket = await createSocketConnection({
-  user: signer.address,
-  socketType: 'chat',
-  socketOptions: { autoConnect: true, reconnectionAttempts: 3 },
-  env: 'prod',
+// Listen for stream
+// Checkout all chat stream listen options - https://push.org/docs/chat/build/stream-chats/
+// Alternatively, just initialize userAlice.stream.initialize() without any listen options to listen to all events
+const stream = await userAlice.stream({
+  listen: [CONSTANTS.STREAM.CHAT, CONSTANTS.STREAM.CONNECT, CONSTANTS.STREAM.DISCONNECT], 
 });
 
-pushSDKSocket.on(EVENTS.CONNECT, (message) => {
-  console.log('Socket Connected');
+stream.on(CONSTANTS.STREAM.CONNECT, () => {
+  console.log('Stream Connected');
   
   // Send a message to Bob after socket connection so that messages as an example
   console.log('Sending message to PushAI Bot');
-  const aliceMessagesPushAI = userAlice.chat.send(pushAIWalletAddress, {
+  userAlice.chat.send(pushAIWalletAddress, {
     content: "Gm gm! It's a me... Mario",
   });
   
 });
 
+stream.on(CONSTANTS.STREAM.DISCONNECT, () => {
+  console.log('Stream Disconnected');
+});
+
 // React to message payload getting recieved
-pushSDKSocket.on(EVENTS.CHAT_RECEIVED_MESSAGE, (message) => {
+stream.on(CONSTANTS.STREAM.CHAT, (message) => {
   console.log('Encrypted Message Received');
   console.log(message);
   
