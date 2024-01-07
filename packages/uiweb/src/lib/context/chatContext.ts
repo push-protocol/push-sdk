6740d8c5e552ef3441d9192c1fdec1d1362f07ea
Pritipriya Singh <mail2pritipriya@gmail.com>
2024-01-08 01:45:53 +0530

    fix: fixed the nomeclature

--- packages/uiweb/src/lib/context/chatContext.ts
+++ packages/uiweb/src/lib/context/chatContext.ts
@@ -1,53 +1,47 @@
-import { Env,IMessageIPFS, IUser, PushAPI, SignerType } from "@pushprotocol/restapi";
+import { Env, IMessageIPFS, IUser, PushAPI, SignerType } from "@pushprotocol/restapi";
 import { Constants } from "../config";
 import { createContext } from "react";
 
 
 export interface IChatDataContextValues {
-    account: string | null;
-    setAccount: React.Dispatch<React.SetStateAction<string| null>>;
-    signer: SignerType | undefined;
-    setSigner: React.Dispatch<React.SetStateAction<SignerType | undefined>>;
-    env: Env;
-    setEnv: React.Dispatch<React.SetStateAction<Env>>;
-    pushStream : any;
-    pushSetStream: React.Dispatch<React.SetStateAction<any>>;
-    isPushChatStreamConnected: boolean;
-    setIsPushChatStreamConnected: React.Dispatch<React.SetStateAction<boolean>>;
-    pushUser: PushAPI | undefined;
-    setPushUser: React.Dispatch<React.SetStateAction<PushAPI>>;
+  account: string | null;
+  setAccount: React.Dispatch<React.SetStateAction<string | null>>;
+  signer: SignerType | undefined;
+  setSigner: React.Dispatch<React.SetStateAction<SignerType | undefined>>;
+  env: Env;
+  setEnv: React.Dispatch<React.SetStateAction<Env>>;
+  isPushChatStreamConnected: boolean;
+  setIsPushChatStreamConnected: React.Dispatch<React.SetStateAction<boolean>>;
+  pushUser: PushAPI | undefined;
+  setPushUser: React.Dispatch<React.SetStateAction<PushAPI>>;
 
 }
 
 export const initialChatDataContextValues: IChatDataContextValues = {
-    account: null,
-    setAccount: () => {
-      /**/
-    },
-    signer: undefined,
-    setSigner: () => {
-      /**/
-    },
-    env: Constants.ENV.PROD,
-    setEnv: () => {
-      /**/
-    },
-    pushStream: null,
-    pushSetStream: () => {
-      /** */
-    },
-    isPushChatStreamConnected: false,
-    setIsPushChatStreamConnected: () => {
-      /** */
-    },
-    pushUser: undefined,
-    setPushUser: () => {
-      /** */
-    },
+  account: null,
+  setAccount: () => {
+    /**/
+  },
+  signer: undefined,
+  setSigner: () => {
+    /**/
+  },
+  env: Constants.ENV.PROD,
+  setEnv: () => {
+    /**/
+  },
+  isPushChatStreamConnected: false,
+  setIsPushChatStreamConnected: () => {
+    /** */
+  },
+  pushUser: undefined,
+  setPushUser: () => {
+    /** */
+  },
 
 }
 
 
 export const ChatDataContext = createContext<IChatDataContextValues>(
-    initialChatDataContextValues
-  );
+  initialChatDataContextValues
+);
---
+++ packages/uiweb/src/lib/hooks/chat/usePushChatStream.ts
@@ -0,0 +1,143 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect, useState } from 'react';
+import { CONSTANTS, PushAPI, SignerType } from '@pushprotocol/restapi';
+import { ENV } from '../../config';
+import { useChatData } from './useChatData';
+
+
+export type PushChatStream = {
+    account?: string | null;
+    env?: ENV;
+};
+
+export const pushChatStream = () => {
+
+
+    const {
+        account,
+        isPushChatStreamConnected,
+        setIsPushChatStreamConnected,
+        env,
+        pushUser
+
+    } = useChatData();
+
+
+    const [chatStream, setChatStream] = useState<any>({}) // to track any new messages
+    const [chatRequestStream, setChatRequestStream] = useState<any>({}); // any message in request
+    const [groupMetaStream, setGroupMetaStream] = useState<any>({}); //group info
+
+
+    const [pushChatstream, setPushChatStream] = useState<any>({});
+    const addSocketEvents = async () => {
+        console.warn('\n--> addChatSocketEvents - stream');
+        pushChatstream?.on(CONSTANTS.STREAM.CONNECT, (err: Error) => {
+            console.log('CONNECTED - stream: ', err);
+            setIsPushChatStreamConnected(true);
+        });
+        await pushChatstream.connect();
+
+        pushChatstream?.on(CONSTANTS.STREAM.DISCONNECT, (err: Error) => {
+            console.log('DIS-CONNECTED: ', err);
+            setIsPushChatStreamConnected(false);
+        });
+
+
+        //Listen for chat messages, your message, request, accept, rejected,
+        console.log('\t-->will attach eachMessage event now - stram');
+        pushChatstream?.on(CONSTANTS.STREAM.CHAT, (message: any) => {
+
+
+            console.log("at streammm", message)
+
+            if ((message.event === "chat.request")) {
+                setChatRequestStream(message);
+            } else {
+                setChatStream((chats: any) => {
+                    return { ...message };
+                });
+            }
+
+            // stream?.disconnect();
+        });
+        pushChatstream?.on(CONSTANTS.STREAM.CHAT_OPS, (chatops: any) => {
+            console.log('Alice received chat ops - stream', chatops);
+            setGroupMetaStream(chatops)
+        });
+        // stream.disconnect();
+
+    };
+
+
+
+    const removeSocketEvents = () => {
+        pushChatstream?.disconnect();
+    };
+
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(() => {
+        if (Object.keys(pushChatstream || {}).length !== 0) {
+            addSocketEvents();
+        }
+
+        return () => {
+            if (Object.keys(pushChatstream || {}).length !== 0) {
+                removeSocketEvents();
+            }
+        }
+    }, [pushChatstream]);
+
+    console.log("checkk", pushChatstream)
+    /**
+     * Whenever the requisite params to create a connection object change
+     *  - disconnect the old connection 
+     *  - create a new connection object
+     */
+
+    useEffect(() => {
+        if (pushUser) {
+            if (Object.keys(pushChatstream || {}).length !== 0) {
+                console.log('=================>>> disconnection in the hook');
+                pushChatstream?.disconnect();
+            } else {
+                const main = async () => {
+                    console.log("initializngggg....")
+                    const newstream = await pushUser?.initStream(
+                        [
+                            CONSTANTS.STREAM.CHAT,
+                            CONSTANTS.STREAM.CHAT_OPS,
+                            CONSTANTS.STREAM.CONNECT,
+                            CONSTANTS.STREAM.DISCONNECT,
+
+                        ],
+                        {
+
+                            connection: {
+                                retries: 3, // number of retries in case of error
+                            },
+                            raw: true   
+                        }
+                    );
+                    // await pushChatStream.connect();
+                    console.log('new connection object: ---- ', newstream);
+                    setPushChatStream(newstream);
+
+                };
+                main().catch((err) =>
+                    console.log("error initializing the stream", err)
+                );
+            }
+
+
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [account, env, pushUser, isPushChatStreamConnected, pushChatstream]);
+
+    return {
+        chatStream,
+        groupMetaStream,
+        chatRequestStream,
+        isPushChatStreamConnected
+    }
+};
