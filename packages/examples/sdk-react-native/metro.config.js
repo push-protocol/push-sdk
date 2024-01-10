508ae66c3050c372ff0dd96847f06bede0f1b96f
kalashshah <202051096@iiitvadodara.ac.in>
2024-01-10 18:16:10 +0530

    chore: link rn packages to reflect changes in example app

--- packages/examples/sdk-react-native/metro.config.js
+++ packages/examples/sdk-react-native/metro.config.js
@@ -1,11 +1,26 @@
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
+const path = require('path');
+
+const reactnativesdkPath = path.resolve(__dirname, '../../reactnative');
+const uireactnativePath = path.resolve(__dirname, '../../uireactnative'); // Path of your local module
+
+const thirdPartyPackages = {
+  '@pushprotocol/uireactnative': uireactnativePath,
+  '@pushprotocol/react-native-sdk': reactnativesdkPath,
+};
+
 /**
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  resolver: {
+    thirdPartyPackages,
+  },
+  watchFolders: [uireactnativePath, reactnativesdkPath],
+};
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
