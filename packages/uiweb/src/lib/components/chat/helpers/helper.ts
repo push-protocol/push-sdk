56ce321cced77689153600e4ef0db8bdf6d830ae
Monalisha Mishra <42746736+mishramonalisha76@users.noreply.github.com>
2024-03-05 12:11:58 +0530

    Revert "fix: added nonce for chat list api call (#1013)"
    This reverts commit 11df22798ce5e8e857df597cc850967d084fd74b.

--- packages/uiweb/src/lib/components/chat/helpers/helper.ts
+++ packages/uiweb/src/lib/components/chat/helpers/helper.ts
@@ -113,14 +113,3 @@ export const checkIfAccessVerifiedGroup = (chatFeed: IFeeds) => {
   }
   return isRules;
 };
-
-export const generateRandomNonce: () => string = () => {
-  let text = "";
-  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for(let i = 0; i < 32; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-
-  return text;
-};
