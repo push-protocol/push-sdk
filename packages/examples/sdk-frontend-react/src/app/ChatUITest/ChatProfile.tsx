e9815656f76f99d841e0895ec4fc283abd04db16
Monalisha Mishra <42746736+mishramonalisha76@users.noreply.github.com>
2024-03-21 12:30:43 +0530

    fix: add doman in user profile and chat profile and copy (#1171)
    * fix: add doman in user profile and chat profile and copy
    
    * fix: fixed tooltip

--- packages/examples/sdk-frontend-react/src/app/ChatUITest/ChatProfile.tsx
+++ packages/examples/sdk-frontend-react/src/app/ChatUITest/ChatProfile.tsx
@@ -7,7 +7,7 @@ export const ChatProfileTest = () => {
             <ChatProfile 
                 chatProfileLeftHelperComponent={<div>left component</div>}
                 chatProfileRightHelperComponent={<div>right component</div>}
-                chatId='eip155:0x455E5AA18469bC6ccEF49594645666C587A3a71B'
+                chatId='4ac5ab85c9c3d57adbdf2dba79357e56b2f9ef0256befe750d9f93af78d2ca68'
 // chatId='36baf37e441fdd94e23406c6c716fc4e91a93a9ee68e070cd5b054534dbe09a6'
             />
         </div>
