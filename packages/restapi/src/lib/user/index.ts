b7882fc50a258f4d4824c8bd1a45b6761cdcd86f
rohitmalhotra1420 <rohit.malhotra1420@gmail.com>
2024-05-07 14:32:05 +0530

    refactor(multiple files): fixes export related termnial warning and updates yarn.lock

--- packages/restapi/src/lib/user/index.ts
+++ packages/restapi/src/lib/user/index.ts
@@ -1,6 +1,6 @@
 import { authUpdate } from './auth.updateUser';
 import { profileUpdate, profileUpdateCore } from './profile.updateUser';
-export { ProfileUpdateProps } from './profile.updateUser';
+export type { ProfileUpdateProps } from './profile.updateUser';
 export * from './createUser';
 export * from './getFeeds';
 export * from './getSubscriptions';
--- packages/uiweb/src/lib/components/space/index.ts
+++ packages/uiweb/src/lib/components/space/index.ts
@@ -1,15 +1,9 @@
-import { ISpaceBannerProps } from "./SpaceBanner";
-import { ISpaceFeedProps } from "./SpaceFeed";
-import { SpacesUI } from "./SpacesUI";
-import { ISpaceInvitesProps } from "./SpaceInvites";
-import { ISpaceCreateWidgetProps } from "./SpaceCreationWidget";
+import { ISpaceBannerProps } from './SpaceBanner';
+import { ISpaceFeedProps } from './SpaceFeed';
+import { ISpaceInvitesProps } from './SpaceInvites';
+import { ISpaceCreateWidgetProps } from './SpaceCreationWidget';
 
-export {
-  SpacesUI,
-  ISpaceBannerProps,
-  ISpaceFeedProps,
-  ISpaceInvitesProps,
-  ISpaceCreateWidgetProps
-}
+export { SpacesUI } from './SpacesUI';
+export type { ISpaceBannerProps, ISpaceFeedProps, ISpaceInvitesProps, ISpaceCreateWidgetProps };
 
 export * from './exportedTypes';
