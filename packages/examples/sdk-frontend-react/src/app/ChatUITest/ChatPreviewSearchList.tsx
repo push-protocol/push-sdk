f65c2359f360925fef63096ae2f6aa602a4338f6
Monalisha Mishra <mishramonalisha76@gmail.com>
2024-05-16 17:11:43 +0530

    fix: fixed review comment

--- packages/examples/sdk-frontend-react/src/app/ChatUITest/ChatPreviewSearchList.tsx
+++ packages/examples/sdk-frontend-react/src/app/ChatUITest/ChatPreviewSearchList.tsx
@@ -1,19 +1,20 @@
 import { ChatPreviewSearchList } from '@pushprotocol/uiweb';
+import styled from 'styled-components';
 
 const ChatPreviewSearchListTest = () => {
   return (
     <>
-      <div
-        style={{
-          background: '#ffeded',
-          border: '1px solid rgb(226,8,128)',
-          height: '28.5vh',
-        }}
-      >
+      <Conatiner>
         <ChatPreviewSearchList searchParamter="0xFA3F8E79fb9B03e7a04295594785b91588Aa4DC8" />
-      </div>
+      </Conatiner>
     </>
   );
 };
 
 export default ChatPreviewSearchListTest;
+
+const Conatiner = styled.div`
+background: '#ffeded',
+border: '1px solid rgb(226,8,128)',
+height: '28.5vh',
+`;
