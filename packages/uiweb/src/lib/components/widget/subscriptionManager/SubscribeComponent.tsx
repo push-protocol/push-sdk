1cfec2150763bfdb9340f937fc7b2c58298f610c
Monalisha Mishra <42746736+mishramonalisha76@users.noreply.github.com>
2024-05-22 18:31:18 +0530

    fix: fixed support chat init issue (#1292)
    * fix: fixed support chat init issue
    
    * fix: fixed lint errors

--- packages/uiweb/src/lib/components/widget/subscriptionManager/SubscribeComponent.tsx
+++ packages/uiweb/src/lib/components/widget/subscriptionManager/SubscribeComponent.tsx
@@ -7,7 +7,7 @@ import { useManageSubscriptionsUtilities, useWidgetData } from '../../../hooks';
 import useToast from '../reusables/NewToast';
 import { MdCheckCircle, MdError } from 'react-icons/md';
 import { ThemeContext } from '../theme/ThemeProvider';
-import { ConnectButtonComp } from '../ConnectButton';
+import { ConnectButton } from '../ConnectButton';
 import { WidgetErrorCodes } from './types';
 import { SettingsComponent } from './SettingsComponents';
 import { notifUserSettingFormatString, notifUserSettingFromChannelSetting } from '../helpers';
@@ -22,10 +22,8 @@ import { ChannelDetailsComponent } from './ChannelDetailsComponent';
 // }
 
 const SubHeadingText = {
-  withSettings:
-    'Select which setting list you would like to receive notifications from.',
-  withoutSettings:
-    'Subscribe and receive notifications from your favorite protocol.',
+  withSettings: 'Select which setting list you would like to receive notifications from.',
+  withoutSettings: 'Subscribe and receive notifications from your favorite protocol.',
 };
 interface ISubscribeComponentProps {
   autoconnect?: boolean;
@@ -33,37 +31,28 @@ interface ISubscribeComponentProps {
   channelAddress: string;
   handleNext: () => void;
 }
-export const SubscribeComponent: React.FC<ISubscribeComponentProps> = (
-  options: ISubscribeComponentProps
-) => {
-  const {
-    channelInfo,
-    handleNext,
-    channelAddress,
-    autoconnect = false,
-  } = options || {};
-  const { subscribeToChannel, subscribeError, subscribeLoading,setSubscribeError } =
-    useManageSubscriptionsUtilities();
+export const SubscribeComponent: React.FC<ISubscribeComponentProps> = (options: ISubscribeComponentProps) => {
+  const { channelInfo, handleNext, channelAddress, autoconnect = false } = options || {};
+  const { subscribeToChannel, subscribeError, subscribeLoading, setSubscribeError } = useManageSubscriptionsUtilities();
   const theme = useContext(ThemeContext);
-  const { signer, setAccount, setSigner,user,account } = useWidgetData();
+  const { signer, setAccount, setSigner, user, account } = useWidgetData();
   const subscribeToast = useToast();
-  const [modifiedSettings, setModifiedSettings] = useState<NotificationSettingType[]|null>(useMemo(() => {
-    if (channelInfo && channelInfo?.channel_settings) {
-      return notifUserSettingFromChannelSetting({settings:( JSON.parse(channelInfo?.channel_settings))});
-    }
-    return null;
-  }, [channelInfo]));
-
- 
+  const [modifiedSettings, setModifiedSettings] = useState<NotificationSettingType[]>(
+    useMemo(() => {
+      if (channelInfo && channelInfo?.channel_settings) {
+        return notifUserSettingFromChannelSetting({ settings: JSON.parse(channelInfo?.channel_settings) });
+      }
+      return [];
+    }, [channelInfo])
+  );
 
   const handleSubscribe = async () => {
- 
     try {
       const response = await subscribeToChannel({
         channelAddress: channelAddress,
-        channelSettings:notifUserSettingFormatString({
-            settings: modifiedSettings!,
-          }),
+        channelSettings: notifUserSettingFormatString({
+          settings: modifiedSettings,
+        }),
       });
       if (response && response?.status === 204) {
         //show toast
@@ -72,70 +61,100 @@ export const SubscribeComponent: React.FC<ISubscribeComponentProps> = (
           toastTitle: 'Notifications Enabled',
           toastMessage: `You have successfully enabled notifications from ${channelInfo?.name}`,
           toastType: 'SUCCESS',
-          getToastIcon: (size) => <MdCheckCircle size={size} color="green" />,
+          getToastIcon: (size) => (
+            <MdCheckCircle
+              size={size}
+              color="green"
+            />
+          ),
         });
-    
       }
     } catch (e) {
       console.debug(e);
-      setSubscribeError(
-        WidgetErrorCodes.NOTIFICATION_WIDGET_SUBSCRIBE_ERROR
-      );
+      setSubscribeError(WidgetErrorCodes.NOTIFICATION_WIDGET_SUBSCRIBE_ERROR);
     }
     if (subscribeError) {
       subscribeToast.showMessageToast({
         toastTitle: 'Error while Enabling Notifications',
-        toastMessage:
-          'We encountered an error while enabling notifications. Please try again.',
+        toastMessage: 'We encountered an error while enabling notifications. Please try again.',
         toastType: 'ERROR',
-        getToastIcon: (size) => <MdError size={size} color="red" />,
+        getToastIcon: (size) => (
+          <MdError
+            size={size}
+            color="red"
+          />
+        ),
       });
     }
   };
   return (
-    <Section flexDirection="column" gap="10px" margin="14px 10px 0px 10px">
-        <Section
-          flexDirection="column"
-          alignItems="start"
-          gap="5px"
-          margin="0 0 10px 0"
+    <Section
+      flexDirection="column"
+      gap="10px"
+      margin="14px 10px 0px 10px"
+    >
+      <Section
+        flexDirection="column"
+        alignItems="start"
+        gap="5px"
+        margin="0 0 10px 0"
+      >
+        <Span
+          fontSize="21px"
+          fontWeight="700"
+          color={theme?.textColor?.modalHeaderText}
         >
-          <Span fontSize="21px" fontWeight="700" color={theme?.textColor?.modalHeaderText}>
-            Subscribe to get Notified
-          </Span>
-          <Span
-            fontSize="12px"
-            textAlign="left"
-            fontWeight="400"
-            width="90%"
-            color={theme?.textColor?.modalSubTitleText}
-          >
-            {SubHeadingText.withoutSettings}
-          </Span>
+          Subscribe to get Notified
+        </Span>
+        <Span
+          fontSize="12px"
+          textAlign="left"
+          fontWeight="400"
+          width="90%"
+          color={theme?.textColor?.modalSubTitleText}
+        >
+          {SubHeadingText.withoutSettings}
+        </Span>
+      </Section>
+      <ChannelDetailsComponent channelInfo={channelInfo} />
+      {channelInfo && channelInfo?.channel_settings && (
+        <Section
+          margin=" 0"
+          width="100%"
+        >
+          <SettingsComponent
+            settings={modifiedSettings!}
+            setSettings={setModifiedSettings}
+          />
         </Section>
-        <ChannelDetailsComponent channelInfo={channelInfo}/>
-     {channelInfo && channelInfo?.channel_settings && <Section margin=' 0' width='100%' >
-      <SettingsComponent settings={modifiedSettings!} setSettings={setModifiedSettings}/>
-      </Section>}
-      
-   {(user && !!user?.readmode)?
-   <>
-     {user?.readmode() && (
-        <ConnectButtonComp
-          autoconnect={autoconnect}
-          setAccount={setAccount}
-          setSigner={setSigner}
-          signer={signer}
-        />)}
-         
-       {!user?.readmode() &&  
-      <Button onClick={handleSubscribe}>
-        {subscribeLoading ? <Spinner color="#fff" size="24" /> : 'Subscribe'}
-      </Button>}
-      </>
-     :null
-    }
- 
+      )}
+
+      {user && !!user?.readmode ? (
+        <>
+          {user?.readmode() && (
+            <ConnectButton
+              autoconnect={autoconnect}
+              setAccount={setAccount}
+              setSigner={setSigner}
+              signer={signer}
+            />
+          )}
+
+          {!user?.readmode() && (
+            <Button onClick={handleSubscribe}>
+              {subscribeLoading ? (
+                <Spinner
+                  color="#fff"
+                  size="24"
+                />
+              ) : (
+                'Subscribe'
+              )}
+            </Button>
+          )}
+        </>
+      ) : null}
+
       <PoweredByPush />
     </Section>
   );
