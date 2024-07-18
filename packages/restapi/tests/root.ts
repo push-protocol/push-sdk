e2b1b2f634850234d7e466c68d64a6531ee60f98
aman035 <guptaaman200115@gmail.com>
2024-07-18 18:27:14 +0530

    chore: fix tc running

--- packages/restapi/tests/root.ts
+++ packages/restapi/tests/root.ts
@@ -3,23 +3,25 @@ import * as path from 'path';
 
 export const mochaHooks = {
   // This file is needed to end the test suite.
-    beforeAll: [
-      async function () {
+  beforeAll: [
+    async function () {
       // Load .env file
-      const envFound = dotenv.config({ path: path.resolve(__dirname, './.env')})
+      const envFound = dotenv.config({
+        path: path.resolve(__dirname, './.env'),
+      });
       // check if .env exists
-      if (!envFound) {
-        console.log('     .env NOT FOUND     ');
-        process.exit(1);
-      } else {
-        // Check environment setup first
-        console.log('     Verifying ENV     ')
-        const EnvVerifierLoader = (await require('./loaders/envVerifier')).default
-        await EnvVerifierLoader()
-        console.log('     ENV Verified / Generated and Loaded!     ')
-      }
-      },
-    ],
+      // if (!envFound) {
+      //   console.log('     .env NOT FOUND     ');
+      //   process.exit(1);
+      // } else {
+      //   // Check environment setup first
+      //   console.log('     Verifying ENV     ')
+      //   const EnvVerifierLoader = (await require('./loaders/envVerifier')).default
+      //   await EnvVerifierLoader()
+      //   console.log('     ENV Verified / Generated and Loaded!     ')
+      // }
+    },
+  ],
 
   afterAll(done: () => void) {
     done();
